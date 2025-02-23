c5bbc456afedfa0b874fe955e14ce3f80936ca82
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-22 22:54:58 -0500

    contact me form complete

--- src/components/contact-me/ContactForm.jsx
+++ src/components/contact-me/ContactForm.jsx
@@ -1,21 +1,77 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  //   const handleSuccess = (e) => {};
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_1ndabos", "template_zw5aaym", form.current, {
+        publicKey: "Zvy1PqdIt1mJfZyQw",
+      })
+      .then(
+        () => {
+          setName("");
+          setEmail("");
+          setMessage("");
+          setSuccess("Message Sent");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        },
+      );
+  };
+
   return (
     <div>
-      <form action="" className="flex flex-col gap-4">
+      <p className="text-cyan">{success}</p>
+      <form
+        action=""
+        className="flex flex-col gap-4 text-white"
+        ref={form}
+        onSubmit={sendEmail}
+      >
         <input
           type="text"
+          name="from_name"
+          value={name}
+          onChange={handleName}
           placeholder="Your Name"
           required
           className="bg-lightBrown h-12 rounded-lg px-2"
         />
         <input
           type="email"
+          name="from_email"
+          value={email}
+          onChange={handleEmail}
           placeholder="Your Email"
           required
           className="bg-lightBrown h-12 rounded-lg px-2"
         />
         <textarea
           type="text"
+          name="message"
+          value={message}
+          onChange={handleMessage}
           placeholder="Message"
           rows="9"
           cols="50"
--- src/components/contact-me/ContactMeLeft.jsx
+++ src/components/contact-me/ContactMeLeft.jsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 const ContactMeLeft = () => {
   return (
     <div>
-      <div>
+      <div className="flex w-full flex-col gap-8">
         <h2 className="text-orange mb-4 text-3xl">Get in Touch</h2>
         <p className="text-white">
           Feel free to reach out if you'd like to collaborate
@@ -11,7 +11,7 @@ const ContactMeLeft = () => {
           you are just few clicks away!
         </p>
       </div>
-      <ContactForm/>
+      <ContactForm />
     </div>
   );
 };
