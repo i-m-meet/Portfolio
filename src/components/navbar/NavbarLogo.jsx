ab60e728f818017c7afcd96f258d9a2278c17865
Sumeet Sumeet <sumeet.bx1@gmail.com>
2025-02-23 19:13:54 -0500

    scroll bar hidden from browsers

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -2,7 +2,7 @@
 const NavbarLogo = () =>{
   return (
     <div>
-        <h1 className="text-white font-extrabold text-3xl md:block">
+        <h1 className="text-white font-extrabold text-3xl sm:text-xl md:block">
             Sumeet
         </h1>
     </div>
